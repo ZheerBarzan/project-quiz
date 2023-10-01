0ef6d2f3407cac4d6978bd8fcdadfb4807e51c35
ZheerBarzan <zheerbarzan85@outlook.com>
2023-10-01 16:25:55 +0300

    seperation of concern

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import q from "./data/quizes.json"
 import {ref, watch} from "vue"
+import Card from "./components/Card.vue"
 
 
 const search = ref("")
@@ -11,14 +12,7 @@ watch(search, ()=> {
 
 })
 
-
-
 </script>
-
-
-
-
-
 
 <template>
     <div class="container">
@@ -27,13 +21,8 @@ watch(search, ()=> {
         <input v-model.trim="search" type="text" placeholder="Search Quizes...">
         </header>
         <div class="options-container">
-        <div v-for="quiz in quizes" :key="quiz.id" class="card">
-            <img :src="quiz.img" alt="">
-            <div class="card-text">
-            <h2>{{quiz.name}}</h2>
-            <p>{{quiz.questions.length}} questions</p>
-            </div>
-        </div>
+            <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+        
         
         </div>
         
@@ -44,8 +33,10 @@ watch(search, ()=> {
 <style scoped>
 
 .container{
-    max-width: 1200px;
-    margin: 0 auto;
+    max-width: 80%;
+    justify-content: center;
+    align-items: center;
+    margin: 0 0 0 15% ;
 }
 header{
     margin-top: 30px;
@@ -71,29 +62,6 @@ width: fit-content;
     margin-top: 40px;
 }
 
-/* card */
 
-.card{
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-    margin-bottom: 35px;
-    margin-right: 25px;
-    cursor: pointer;
-}
-.card img{
-    width: 100%;
-    height: 190px;
-    margin: 0;
-}
-.card .card-text{
-    padding: 0 5px;
-}
-.card .card-text h2{
-    font-weight: bold;
-}
-.card .card-text p{
-    margin-bottom: 0.3rem;
-}
+
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<script setup>
+import {defineProps} from "vue"
+
+const {quiz} = defineProps(['quiz'])
+
+
+</script>
+
+<template>
+    <div class="card">
+        <img :src="quiz.img" alt="">
+            <div class="card-text">
+            <h2>{{quiz.name}}</h2>
+            <p>{{quiz.questions.length}} questions</p>
+            </div>
+        </div>
+</template>
+
+
+
+<style scoped>
+
+.card{
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 35px;
+    margin-right: 25px;
+    cursor: pointer;
+}
+.card img{
+    width: 100%;
+    height: 190px;
+    margin: 0;
+}
+.card .card-text{
+    padding: 0 5px;
+}
+.card .card-text h2{
+    font-weight: bold;
+}
+.card .card-text p{
+    margin-bottom: 0.3rem;
+}
+</style>
